5464a709cd662770c23faa565b67fa626d50d127
Henao13 <shenaor4@eafit.edu.co>
2024-09-04 10:13:15 -0500

    Progreso 1

--- VirtualParking/settings.py
+++ VirtualParking/settings.py
@@ -73,6 +73,8 @@ TEMPLATES = [
     },
 ]
 
+
+
 WSGI_APPLICATION = 'VirtualParking.wsgi.application'
 
 
@@ -121,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
