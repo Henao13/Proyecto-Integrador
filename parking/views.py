040dae3713fc74135a6af10d05d301ca98408161
Henao13 <shenaor4@eafit.edu.co>
2024-09-04 10:02:38 -0500

    Inicio, saldo y logout

--- parking/views.py
+++ parking/views.py
@@ -1,12 +1,15 @@
-# parking/views.py
-
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import UsuarioFrecuente
 from .forms import LoginForm
 
 def home(request):
-    return render(request, 'home.html')
+    usuario = None
+    if 'usuario_id' in request.session:
+        usuario_id = request.session['usuario_id']
+        usuario = UsuarioFrecuente.objects.get(id_usuario=usuario_id)
+    
+    return render(request, 'home.html', {'usuario': usuario})
 
 def user_login(request):
     if request.method == 'POST':
@@ -16,7 +19,6 @@ def user_login(request):
             contraseña = form.cleaned_data['contraseña']
             try:
                 usuario = UsuarioFrecuente.objects.get(id_vehiculo=id_vehiculo, contraseña=contraseña)
-                # Aquí puedes establecer la sesión del usuario
                 request.session['usuario_id'] = usuario.id_usuario
                 return redirect('home')
             except UsuarioFrecuente.DoesNotExist:
@@ -24,3 +26,8 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
+
+def user_logout(request):
+    if 'usuario_id' in request.session:
+        del request.session['usuario_id']
+    return redirect('home')
