d5116c24187e48aefab4120ecd104aa742909fea
mlondono13 <mlondonol@eafit.edu.co>
2024-09-08 19:15:29 -0500

    Agregar monto al saldo

--- parking/views.py
+++ parking/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.http import JsonResponse
 from .models import UsuarioFrecuente
 from .forms import LoginForm
+from decimal import Decimal, InvalidOperation
 
 def home(request):
     usuario = None
@@ -31,3 +33,29 @@ def user_logout(request):
     if 'usuario_id' in request.session:
         del request.session['usuario_id']
     return redirect('home')
+
+def simular_transaccion_view(request, id_usuario):
+    # Obtener el usuario por su ID
+    user = UsuarioFrecuente.objects.get(id_usuario=id_usuario)
+
+    if request.method == "POST":
+        # Obtener la cantidad desde el formulario
+        cantidad = request.POST.get('cantidad', 0)
+
+        try:
+            # Convertir la cantidad a Decimal
+            cantidad = Decimal(cantidad)
+
+            # Aumentar el saldo del usuario
+            user.saldo += cantidad
+            user.save()
+
+            # Devolver la nueva cantidad como respuesta JSON
+            return JsonResponse({"nuevo saldo": str(user.saldo)})  # Convertir a string para JSON
+
+        except (ValueError, InvalidOperation):
+            # Manejar el caso en que la cantidad no sea un número válido
+            return JsonResponse({"error": "La cantidad debe ser un número válido."}, status=400)
+
+    # Renderizar la plantilla y pasar el objeto usuario al contexto
+    return render(request, 'saldo.html', {'user': user})
